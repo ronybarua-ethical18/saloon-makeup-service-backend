27510a68e13d31db12a461bc54967fc1a610c8e9
Rony <ronybarua-ethical18>
2024-06-15 11:37:19 +0600

    dcoker file and compose file created

--- dist/modules/services/service.route.js
+++ dist/modules/services/service.route.js
@@ -1,20 +1,70 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SaloonServiceRoutes = void 0;
-const express_1 = __importDefault(require("express"));
-const auth_1 = __importDefault(require("../../middlewares/auth"));
-const user_enum_1 = require("../../shared/enums/user.enum");
-const service_controller_1 = require("./service.controller");
-const validateRequest_1 = __importDefault(require("../../middlewares/validateRequest"));
-const service_validation_1 = require("./service.validation");
-const router = express_1.default.Router();
-router.post('/', (0, auth_1.default)(user_enum_1.ENUM_USER_ROLE.SELLER), (0, validateRequest_1.default)(service_validation_1.ServiceValidation.ServiceZodSchema), service_controller_1.SaloonServiceController.createService);
-router.post('/update-many', (0, auth_1.default)(user_enum_1.ENUM_USER_ROLE.SUPER_ADMIN, user_enum_1.ENUM_USER_ROLE.ADMIN), service_controller_1.SaloonServiceController.updateManyServices);
-router.get('/', (0, auth_1.default)(user_enum_1.ENUM_USER_ROLE.SELLER, user_enum_1.ENUM_USER_ROLE.ADMIN, user_enum_1.ENUM_USER_ROLE.SUPER_ADMIN), service_controller_1.SaloonServiceController.getAllServices);
-router.get('/:serviceId', (0, auth_1.default)(user_enum_1.ENUM_USER_ROLE.SELLER, user_enum_1.ENUM_USER_ROLE.ADMIN, user_enum_1.ENUM_USER_ROLE.SUPER_ADMIN), service_controller_1.SaloonServiceController.getService);
-router.patch('/:serviceId', (0, auth_1.default)(user_enum_1.ENUM_USER_ROLE.SELLER, user_enum_1.ENUM_USER_ROLE.ADMIN, user_enum_1.ENUM_USER_ROLE.SUPER_ADMIN), service_controller_1.SaloonServiceController.updateService);
-router.delete('/:serviceId', (0, auth_1.default)(user_enum_1.ENUM_USER_ROLE.SELLER, user_enum_1.ENUM_USER_ROLE.ADMIN, user_enum_1.ENUM_USER_ROLE.SUPER_ADMIN), service_controller_1.SaloonServiceController.deleteService);
-exports.SaloonServiceRoutes = router;
+'use strict'
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod }
+  }
+Object.defineProperty(exports, '__esModule', { value: true })
+exports.SaloonServiceRoutes = void 0
+const express_1 = __importDefault(require('express'))
+const auth_1 = __importDefault(require('../../middlewares/auth'))
+const user_enum_1 = require('../../shared/enums/user.enum')
+const service_controller_1 = require('./service.controller')
+const validateRequest_1 = __importDefault(
+  require('../../middlewares/validateRequest'),
+)
+const service_validation_1 = require('./service.validation')
+const router = express_1.default.Router()
+router.post(
+  '/',
+  (0, auth_1.default)(user_enum_1.ENUM_USER_ROLE.SELLER),
+  (0, validateRequest_1.default)(
+    service_validation_1.ServiceValidation.ServiceZodSchema,
+  ),
+  service_controller_1.SaloonServiceController.createService,
+)
+router.post(
+  '/update-many',
+  (0, auth_1.default)(
+    user_enum_1.ENUM_USER_ROLE.SUPER_ADMIN,
+    user_enum_1.ENUM_USER_ROLE.ADMIN,
+  ),
+  service_controller_1.SaloonServiceController.updateManyServices,
+)
+router.get(
+  '/',
+  (0, auth_1.default)(
+    user_enum_1.ENUM_USER_ROLE.SELLER,
+    user_enum_1.ENUM_USER_ROLE.ADMIN,
+    user_enum_1.ENUM_USER_ROLE.SUPER_ADMIN,
+  ),
+  service_controller_1.SaloonServiceController.getAllServices,
+)
+router.get(
+  '/:serviceId',
+  (0, auth_1.default)(
+    user_enum_1.ENUM_USER_ROLE.SELLER,
+    user_enum_1.ENUM_USER_ROLE.ADMIN,
+    user_enum_1.ENUM_USER_ROLE.SUPER_ADMIN,
+  ),
+  service_controller_1.SaloonServiceController.getService,
+)
+router.patch(
+  '/:serviceId',
+  (0, auth_1.default)(
+    user_enum_1.ENUM_USER_ROLE.SELLER,
+    user_enum_1.ENUM_USER_ROLE.ADMIN,
+    user_enum_1.ENUM_USER_ROLE.SUPER_ADMIN,
+  ),
+  service_controller_1.SaloonServiceController.updateService,
+)
+router.delete(
+  '/:serviceId',
+  (0, auth_1.default)(
+    user_enum_1.ENUM_USER_ROLE.SELLER,
+    user_enum_1.ENUM_USER_ROLE.ADMIN,
+    user_enum_1.ENUM_USER_ROLE.SUPER_ADMIN,
+  ),
+  service_controller_1.SaloonServiceController.deleteService,
+)
+exports.SaloonServiceRoutes = router
--- dist/utils/auth.utils.js
+++ dist/utils/auth.utils.js
@@ -1,21 +1,32 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendMailWithToken = void 0;
-const config_1 = __importDefault(require("../config"));
-const jwtHelpers_1 = require("../helpers/jwtHelpers");
-const sendMail_1 = __importDefault(require("../services/mail/sendMail"));
+'use strict'
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod }
+  }
+Object.defineProperty(exports, '__esModule', { value: true })
+exports.sendMailWithToken = void 0
+const config_1 = __importDefault(require('../config'))
+const jwtHelpers_1 = require('../helpers/jwtHelpers')
+const sendMail_1 = __importDefault(require('../services/mail/sendMail'))
 const sendMailWithToken = (user, subject, emailType, emailTemplate) => {
-    const token = jwtHelpers_1.jwtHelpers.createToken({ userId: user?._id, role: user?.role }, config_1.default.jwt.secret, config_1.default.jwt.expires_in);
-    (0, sendMail_1.default)([user.email], {
-        subject: subject,
-        data: {
-            name: user.firstName + ' ' + user.lastName,
-            role: user.role,
-            token: config_1.default.client_port + '' + `/${emailType}?token=${token}`,
-        },
-    }, emailTemplate);
-};
-exports.sendMailWithToken = sendMailWithToken;
+  const token = jwtHelpers_1.jwtHelpers.createToken(
+    { userId: user?._id, role: user?.role },
+    config_1.default.jwt.secret,
+    config_1.default.jwt.expires_in,
+  )
+  ;(0, sendMail_1.default)(
+    [user.email],
+    {
+      subject: subject,
+      data: {
+        name: user.firstName + ' ' + user.lastName,
+        role: user.role,
+        token:
+          config_1.default.client_port + '' + `/${emailType}?token=${token}`,
+      },
+    },
+    emailTemplate,
+  )
+}
+exports.sendMailWithToken = sendMailWithToken
