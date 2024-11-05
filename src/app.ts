15fbc2a8e8276f1986976c1b1f3787db5473d09c
Rony <ronybarua-ethical18>
2024-11-05 19:49:17 +0600

    booking system with overall core feature is finished

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ app.use('/api/v1', routes)
 
 //Testing
 app.get('/', async (req: Request, res: Response) => {
-  res.send('Working Successfully With github actions integration server')
+  res.send('App is working with fully functional booking system feature.')
 })
 
 //handle not found
