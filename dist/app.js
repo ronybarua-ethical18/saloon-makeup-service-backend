bcf59c994ee8de1066148339af6a1416a0975c3a
Rony <ronybarua-ethical18>
2024-11-05 19:55:47 +0600

    booking system with overall core feature is finished

--- dist/app.js
+++ dist/app.js
@@ -29,7 +29,7 @@ app.use((0, express_mongo_sanitize_1.default)());
 app.use('/api/v1', routes_1.default);
 //Testing
 app.get('/', async (req, res) => {
-    res.send('Working Successfully With github actions integration server');
+    res.send('App is working with fully functional booking system feature.');
 });
 //handle not found
 app.use((req, res, next) => {
